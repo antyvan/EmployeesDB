f96b02286fd5ba29620313da23e2eba0288319e2
Anton Ivanov <antyvan2000@gmail.com>
2018-06-02 18:21:01 +0300

    Test

---
+++ CreateTables.sql
@@ -0,0 +1,119 @@
+USE [EmployeesDB];
+GO
+
+-- Remove objects first
+IF OBJECT_ID('EmployeesDB.dbo.EmployeeProject') is not null
+BEGIN
+	DROP TABLE [dbo].[EmployeeProject]
+END
+
+IF OBJECT_ID('EmployeesDB.dbo.Project') is not null
+BEGIN
+	DROP TABLE [dbo].[Project]
+END
+
+IF OBJECT_ID('EmployeesDB.dbo.EmployeePosition') is not null
+BEGIN
+	DROP TABLE [dbo].[EmployeePosition]
+END
+
+IF OBJECT_ID('EmployeesDB.dbo.Position') is not null
+BEGIN
+	DROP TABLE [dbo].[Position]
+END
+
+IF OBJECT_ID('EmployeesDB.dbo.Salary') is not null
+BEGIN
+	DROP TABLE [dbo].[Salary]
+END
+
+IF OBJECT_ID('EmployeesDB.dbo.WorkPeriod') is not null
+BEGIN
+	DROP TABLE [dbo].[WorkPeriod]
+END
+
+IF OBJECT_ID('EmployeesDB.dbo.Employee') is not null
+BEGIN
+	DROP TABLE [dbo].[Employee]
+END
+
+GO
+
+--************************************** [dbo].[Employee]
+
+CREATE TABLE [dbo].[Employee]
+(
+ [EmployeeId] INT IDENTITY(1,1) PRIMARY KEY,
+ [FirstName]  VARCHAR(40) NOT NULL,
+ [LastName]   VARCHAR(40) NOT NULL,
+ [SuperiorId] INT NULL FOREIGN KEY REFERENCES [dbo].[Employee](EmployeeId)
+);
+
+GO
+
+--************************************** [dbo].[Project]
+
+CREATE TABLE [dbo].[Project]
+(
+ [ProjectId] INT IDENTITY(1,1) PRIMARY KEY,
+ [ProjectName] VARCHAR(40) NOT NULL
+);
+
+GO
+
+--************************************** [dbo].[EmployeeProject]
+
+CREATE TABLE [dbo].[EmployeeProject]
+(
+ [EmployeeId]    INT NOT NULL FOREIGN KEY REFERENCES [dbo].[Employee](EmployeeId),
+ [ProjectId]     INT NOT NULL FOREIGN KEY REFERENCES [dbo].[Project](ProjectId),
+ [EffectiveFrom] DATE NOT NULL,
+ [EffectiveTo]   DATE NULL
+);
+
+GO
+
+--************************************** [dbo].[Position]
+
+CREATE TABLE [dbo].[Position]
+(
+ [PositionId] INT IDENTITY(1,1) PRIMARY KEY,
+ [PositionName] VARCHAR(40) NOT NULL
+);
+
+GO
+
+--************************************** [dbo].[EmployeePosition]
+
+CREATE TABLE [dbo].[EmployeePosition]
+(
+ [EmployeeId]    INT NOT NULL FOREIGN KEY REFERENCES [dbo].[Employee](EmployeeId),
+ [PositionId]    INT NOT NULL FOREIGN KEY REFERENCES [dbo].[Position](PositionId),
+ [EffectiveFrom] DATE NOT NULL,
+ [EffectiveTo]   DATE NULL
+);
+
+GO
+
+--************************************** [dbo].[Salary]
+
+CREATE TABLE [dbo].[Salary]
+(
+ [EmployeeId]    INT NOT NULL FOREIGN KEY REFERENCES [dbo].[Employee](EmployeeId),
+ [MonthlyPay]    MONEY NOT NULL,
+ [EffectiveFrom] DATE NOT NULL,
+ [EffectiveTo]   DATE NULL
+);
+
+GO
+
+--************************************** [dbo].[WorkPeriod]
+
+CREATE TABLE [dbo].[WorkPeriod]
+(
+ [EmployeeId]    INT NOT NULL FOREIGN KEY REFERENCES [dbo].[Employee](EmployeeId),
+ [EffectiveFrom] DATE NOT NULL,
+ [EffectiveTo]   DATE NULL
+);
+
+GO
---
+++ PopulateStaticTables.sql
@@ -0,0 +1,18 @@
+USE [EmployeesDB];
+GO
+
+INSERT INTO [dbo].[Position] (PositionName) VALUES
+('Software Developer'),
+('QA Test Engineer'),
+('Business Analyst'),
+('Project Manager'),
+('Chief Excutive Officer')
+
+GO
+
+INSERT INTO [dbo].[Project] (ProjectName) VALUES
+('Project A'),
+('Project B'),
+('Company Management')
+
+GO
