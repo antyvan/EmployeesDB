524418d574df13905d7dc1e8770d0af9cdcc47c5
Anton Ivanov <antyvan2000@gmail.com>
2018-06-03 22:56:25 +0300

    Add dbo.UpdateEmplyeeSuperior sp.

---
+++ dbo.UpdateEmployeeSuperior.sql
@@ -0,0 +1,23 @@
+﻿USE [EmployeesDB];
+GO
+
+IF OBJECT_ID('[dbo].[UpdateEmployeeSuperior]', 'P') IS NOT NULL
+BEGIN
+	DROP PROCEDURE [dbo].[UpdateEmployeeSuperior]
+END
+
+GO
+
+CREATE PROCEDURE [dbo].[UpdateEmployeeSuperior]
+	@EmployeeId INT,
+	@SuperiorId INT
+AS
+SET NOCOUNT ON; -- Don't report the number of processed row to client
+
+IF @EmployeeId IS NULL
+	RAISERROR('@EmployeeId can''t be NULL', 11, 1)
+
+IF (ISNULL(@SuperiorId, 0) != ISNULL((SELECT SuperiorId FROM dbo.Employee WHERE EmployeeId = @EmployeeId), 0))
+	UPDATE dbo.Employee SET SuperiorId = @SuperiorId WHERE EmployeeId = @EmployeeId
+
+GO
