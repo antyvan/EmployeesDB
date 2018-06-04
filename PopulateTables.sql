eaa5582aa322cd6112e8faeeb6f93a6ca191ddae
Anton Ivanov <antyvan2000@gmail.com>
2018-06-04 16:35:10 +0300

    Modify StatisticsQueries

--- PopulateTables.sql
+++ PopulateTables.sql
@@ -11,23 +11,33 @@ DELETE dbo.Employee WHERE SuperiorId IS NOT NULL
 DELETE dbo.Employee WHERE SuperiorId IS NULL
 
 DECLARE @CEOId INT
-exec dbo.AddEmployee 'Donald', 'Trump', 5 /*CEO*/, 3 /*Comp. Mgmt*/, default, 10000, default, @CEOId OUT
+exec dbo.AddEmployee 'Donald', 'Trump', 5 /*CEO*/, 3 /*Comp. Mgmt*/, default, 10000, '1 Jan 2015', @CEOId OUT
 
 -- Project A resources
 DECLARE @PMAId INT
-exec dbo.AddEmployee 'Bill', 'Gates', 4 /*PM*/, 1 /*Proj. A*/, @CEOId, 5000, default, @PMAId OUT
+exec dbo.AddEmployee 'Bill', 'Gates', 4 /*PM*/, 1 /*Proj. A*/, @CEOId, 5000, '1 Feb 2015', @PMAId OUT
 
-exec dbo.AddEmployee 'Mickey', 'Mouse', 1 /*SD*/, 1 /*Proj. A*/, @PMAId, 3000
-exec dbo.AddEmployee 'Daisy', 'Mouse', 1 /*SD*/, 1 /*Proj. A*/, @PMAId, 3000
-exec dbo.AddEmployee 'Donald', 'Duck', 2 /*QA*/, 1 /*Proj. A*/, @PMAId, 2000
-exec dbo.AddEmployee 'Goofy', 'Wolf', 3 /*BA*/, 1 /*Proj. A*/, @PMAId, 3500
+exec dbo.AddEmployee 'Mickey', 'Mouse', 1 /*SD*/, 1 /*Proj. A*/, @PMAId, 3000, '15 Feb 2015'
+
+DECLARE @DonaldDuckId INT
+exec dbo.AddEmployee 'Donald', 'Duck', 2 /*QA*/, 1 /*Proj. A*/, @PMAId, 2000, '20 Feb 2015', @DonaldDuckId OUT
+exec dbo.AddEmployee 'Goofy', 'Wolf', 3 /*BA*/, 1 /*Proj. A*/, @PMAId, 3500, '25 Feb 2015'
+exec dbo.AddEmployee 'Daisy', 'Mouse', 1 /*SD*/, 1 /*Proj. A*/, @PMAId, 3000, '20 Mar 2015'
 
 -- Project B resources
 DECLARE @PMBId INT
-exec dbo.AddEmployee 'Jackie', 'Chan', 4 /*PM*/, 2 /*Proj. B*/, @CEOId, 6000, default, @PMBId OUT
+exec dbo.AddEmployee 'Jackie', 'Chan', 4 /*PM*/, 2 /*Proj. B*/, @CEOId, 6000, '1 Jul 2016', @PMBId OUT
 
-exec dbo.AddEmployee 'Samo', 'Hung', 1 /*SD*/, 2 /*Proj. B*/, @PMBId, 2000
-exec dbo.AddEmployee 'Bruce', 'Lee', 2 /*SD*/, 2 /*Proj. B*/, @PMBId, 4000
-exec dbo.AddEmployee 'Chuck', 'Noris', 2 /*QA*/, 2 /*Proj. B*/, @PMBId, 2000
-exec dbo.AddEmployee 'Jet', 'Lee', 3 /*BA*/, 2 /*Proj. B*/, @PMBId, 1000
-exec dbo.AddEmployee 'Panda', 'Kung Fu', 3 /*BA*/, 2 /*Proj. B*/, @PMBId, 5000
+exec dbo.AddEmployee 'Samo', 'Hung', 1 /*SD*/, 2 /*Proj. B*/, @PMBId, 2000, '23 Jul 2016'
+exec dbo.AddEmployee 'Bruce', 'Lee', 2 /*SD*/, 2 /*Proj. B*/, @PMBId, 4000, '1 Sep 2016'
+exec dbo.AddEmployee 'Chuck', 'Noris', 2 /*QA*/, 2 /*Proj. B*/, @PMBId, 2000, '5 Sep 2016'
+exec dbo.AddEmployee 'Jet', 'Lee', 3 /*BA*/, 2 /*Proj. B*/, @PMBId, 1000, '1 Dec 2016'
+exec dbo.AddEmployee 'Panda', 'Kung Fu', 3 /*BA*/, 2 /*Proj. B*/, @PMBId, 5000, '1 Apr 2017'
+
+-- Create career for Donald Duck
+exec dbo.UpdateEmployee @DonaldDuckId, @MonthlyPay = 2100, @EffectiveFrom = '20 May 2015'
+exec dbo.UpdateEmployee @DonaldDuckId, @PositionId = 1 /*SD*/, @EffectiveFrom = '20 Aug 2015'
+exec dbo.UpdateEmployee @DonaldDuckId, @MonthlyPay = 2500, @EffectiveFrom = '1 Nov 2015'
+exec dbo.UpdateEmployee @DonaldDuckId, @ProjectId = 2, @EffectiveFrom = '2 Jul 2016'
+exec dbo.UpdateEmployeeSuperior @DonaldDuckId, @PMBId
+exec dbo.DismissEmployee @DonaldDuckId, '1 Mar 2017'
--- StatisticsQueries.sql
+++ StatisticsQueries.sql
@@ -9,7 +9,7 @@ SELECT
 FROM dbo.EmployeeProject empProj
 	JOIN dbo.Project proj on empProj.ProjectId = proj.ProjectId
 	JOIN dbo.Salary sal on empProj.EmployeeId = sal.EmployeeId and sal.EffectiveTo is NULL
-	JOIN dbo.WorkPeriod wp on empProj.EmployeeId = wp.EmployeeId
+	JOIN dbo.WorkPeriod wp on empProj.EmployeeId = wp.EmployeeId and wp.EffectiveTo is NULL
 WHERE
 	empProj.EffectiveTo is NULL
 GROUP BY proj.ProjectName
@@ -23,10 +23,70 @@ SELECT
 FROM dbo.EmployeePosition empPos
 	JOIN dbo.Position pos on empPos.PositionId = pos.PositionId
 	JOIN dbo.Salary sal on empPos.EmployeeId = sal.EmployeeId and sal.EffectiveTo is NULL
-	JOIN dbo.WorkPeriod wp on empPos.EmployeeId = wp.EmployeeId
+	JOIN dbo.WorkPeriod wp on empPos.EmployeeId = wp.EmployeeId and wp.EffectiveTo is NULL
 WHERE
 	empPos.EffectiveTo is NULL
 GROUP BY pos.PositionName
 ORDER BY pos.PositionName
 
--- SELECT BY SUPERIOR???
+-- SELECT BY SUPERIORS
+SELECT
+	empSup.FirstName,
+	empSup.LastName,
+	COUNT(empSub.EmployeeId) as SubordinateCount,
+	SUM(sal.MonthlyPay) as TotalMonthlyPay
+FROM dbo.Employee empSup
+	JOIN dbo.WorkPeriod wpSup on empSup.EmployeeId = wpSup.EmployeeId and wpSup.EffectiveTo is NULL
+	JOIN dbo.Employee empSub on empSup.EmployeeId = empSub.SuperiorId
+	JOIN dbo.Salary sal on empSub.EmployeeId = sal.EmployeeId and sal.EffectiveTo is NULL
+	JOIN dbo.WorkPeriod wpSub on empSub.EmployeeId = wpSub.EmployeeId and wpSub.EffectiveTo is NULL
+GROUP BY empSup.FirstName, empSup.LastName
+ORDER BY empSup.FirstName, empSup.LastName
+
+-- SELECT GRAND TOTAL BY A SUPERIOR
+DECLARE @SuperiorId INT
+SELECT @SuperiorId = EmployeeId from dbo.Employee WHERE FirstName= 'Donald' and LastName ='Trump'
+
+;WITH cte_Subordinates (EmployeeId)
+AS
+(
+	SELECT @SuperiorId
+	UNION ALL
+	SELECT emp.EmployeeId
+	FROM cte_Subordinates sub
+	JOIN dbo.Employee emp ON sub.EmployeeId = emp.SuperiorId
+)
+SELECT
+	COUNT(sub.EmployeeId) as SubordinateCount,
+	ISNULL(SUM(sal.MonthlyPay), 0) as TotalMonthlyPay
+FROM cte_Subordinates sub
+	JOIN dbo.Salary sal on sub.EmployeeId = sal.EmployeeId and sal.EffectiveTo is NULL
+	JOIN dbo.WorkPeriod wp on sub.EmployeeId = wp.EmployeeId and wp.EffectiveTo is NULL
+WHERE sub.EmployeeId != @SuperiorId
+
+-- SELECT AN EMPLOYEE HISTORY
+DECLARE @EmployeeId INT
+SELECT @EmployeeId = EmployeeId from dbo.Employee WHERE FirstName= 'Donald' and LastName ='Duck'
+
+SELECT
+	sproj.ProjectName,
+	proj.EffectiveFrom,
+	proj.EffectiveTo
+FROM dbo.EmployeeProject proj
+JOIN dbo.Project sproj ON proj.ProjectId = sproj.ProjectId
+WHERE proj.EmployeeId = @EmployeeId
+
+SELECT
+	spos.PositionName,
+	pos.EffectiveFrom,
+	pos.EffectiveTo
+FROM dbo.EmployeePosition pos
+JOIN dbo.Position spos ON pos.PositionId = spos.PositionId
+WHERE pos.EmployeeId = @EmployeeId
+
+SELECT
+	sal.MonthlyPay,
+	sal.EffectiveFrom,
+	sal.EffectiveTo
+FROM dbo.Salary sal
+WHERE sal.EmployeeId = @EmployeeId
